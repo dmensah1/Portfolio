d11b66b0458b367f683796ac18599704a13ddbe9
dmensah1 <dmensah4@uwo.ca>
2020-09-03 15:11:13 -0400

    Finished project view design

--- portfolio-site/src/app/projects/project-list/project-list.component.ts
+++ portfolio-site/src/app/projects/project-list/project-list.component.ts
@@ -12,4 +12,12 @@ export class ProjectListComponent implements OnInit{
   ngOnInit() {
     this.projects = JSON.parse(JSON.stringify(data.projects));
   }
+
+  goToTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
 }
--- portfolio-site/src/app/projects/project-view/project-view.component.ts
+++ portfolio-site/src/app/projects/project-view/project-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import data from '../../../assets/Data.json';
 import { ActivatedRoute } from '@angular/router';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   templateUrl: './project-view.component.html',
@@ -9,16 +10,26 @@ import { ActivatedRoute } from '@angular/router';
 export class ProjectViewComponent implements OnInit{
   projectId: string;
   projects: any;
+  projectImgs;
+  projectSkills;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, config: NgbCarouselConfig) {
     if (this.route.snapshot.params['id']) { // might need to change these to blogId
       this.projectId = this.route.snapshot.paramMap.get('id');
     }
+
+    config.interval = 5000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
   }
 
   ngOnInit() {
    // console.log(this.projectId);
     this.projects = JSON.parse(JSON.stringify(data.projects));
     this.projects = this.projects.filter(project => project.id == this.projectId);
+    this.projectImgs = this.projects[0].pictures;
+    this.projectSkills = this.projects[0].skills;
+    console.log(this.projectSkills);
   }
 }
